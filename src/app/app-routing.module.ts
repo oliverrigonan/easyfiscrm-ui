29a27bd62129bcac36ef63f6e4e3a97d51553ba3
NoahOliverRIgonan <oliverrigonan@gmail.com>
2019-02-05 15:23:04 +0800

    Configure API URL

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './landing/landing.module#LandingModule' },
+  { path: '', loadChildren: './account/account.module#AccountModule' },
   { path: 'landing', loadChildren: './landing/landing.module#LandingModule' },
   { path: 'account', loadChildren: './account/account.module#AccountModule' },
   { path: 'software', loadChildren: './software/software.module#SoftwareModule' }
--- src/app/app-settings.ts
+++ src/app/app-settings.ts
@@ -1,3 +1,4 @@
 export class AppSettings {
-    public defaultAPIURLHost: string = "http://localhost:53129";
+    // public defaultAPIURLHost: string = "http://localhost:53129";
+    public defaultAPIURLHost: string = "http://easyfiscrm-easyfis-api.azurewebsites.net";
 }
